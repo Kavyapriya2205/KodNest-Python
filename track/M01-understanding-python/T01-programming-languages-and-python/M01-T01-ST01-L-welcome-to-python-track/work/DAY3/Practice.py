f0cd49a7d8ae0066fad740d1a99f5ef2c7062d71
Kavyapriya2205 <kavyapriya2205@example.com>
2026-08-04 18:40:00 +0530

    DAY 3 PRACTICE

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/DAY3/Practice.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+opration = input()
+
+if(opration =="add") :      
+  print(a+b)
+elif(opration =="sub"):
+  print(a-b)
+elif(opration =="mul"):
+  print(a*b)
+elif(opration =="div"):
+  print(a/b)
+else:
+  print("Invalid opration")
